50eb71fb0ba2839cfbc7e39f7a594f32a28ff86a
hakekett <hakekett@student.21-school.ru>
2024-12-04 17:49:50 +0300

    part2 done

--- src/part2/part2.sql
+++ src/part2/part2.sql
@@ -57,7 +57,6 @@ AS $$
         IF NEW.state = 'Start' THEN
             UPDATE transferredpoints SET points_amount = points_amount + 1
             WHERE checking_peer = NEW.checking_peer AND checked_peer = (SELECT peer FROM checks WHERE id = NEW.check_id LIMIT 1);
-
             IF NOT FOUND THEN
                 INSERT INTO transferredpoints (checking_peer, checked_peer, points_amount)
                 VALUES (NEW.checking_peer, (SELECT peer FROM checks WHERE id = NEW.check_id LIMIT 1),1);
@@ -71,23 +70,55 @@ CREATE OR REPLACE TRIGGER trg_peer_update_transferred_points
 AFTER INSERT ON p2p
 FOR EACH ROW EXECUTE FUNCTION check_points_update();
 
+INSERT INTO p2p(check_id, checking_peer, state, time) VALUES (4, 'iosfiypdje', 'Failure', '15:00:00');
+INSERT INTO p2p(check_id, checking_peer, state, time) VALUES (4, 'iosfiypdje', 'Start', '11:00:00');
+
 -- 4) A trigger: before adding a record to the XP table, check if it is correct
--- CREATE OR REPLACE FUNCTION check_xp_update() RETURNS trigger
--- AS $$
---     BEGIN
---         IF NEW.xp_amount <= (SELECT xp FROM tasks JOIN checks ON checks.id = NEW.check_id) THEN
---             UPDATE transferredpoints SET points_amount = points_amount + 1
---             WHERE checking_peer = NEW.checking_peer AND checked_peer = (SELECT peer FROM checks WHERE id = NEW.check_id LIMIT 1);
---
---             IF NOT FOUND THEN
---                 INSERT INTO transferredpoints (checking_peer, checked_peer, points_amount)
---                 VALUES (NEW.checking_peer, (SELECT peer FROM checks WHERE id = NEW.check_id LIMIT 1),1);
---             END IF;
---         END IF;
---     RETURN NEW;
---     END;
--- $$ LANGUAGE plpgsql;
---
--- CREATE OR REPLACE TRIGGER trg_xp_update_xp
--- BEFORE INSERT ON xp
--- FOR EACH ROW EXECUTE FUNCTION check_xp_update();
+CREATE OR REPLACE FUNCTION check_xp_update() RETURNS trigger
+AS $$
+DECLARE
+    task_xp INT;
+    verter_state TEXT;
+    p2p_state TEXT;
+    BEGIN
+
+    SELECT xp
+    INTO task_xp
+    FROM tasks
+    JOIN checks ON checks.task = tasks.title
+    WHERE checks.id = NEW.check_id;
+
+    IF NEW.xp_amount <= task_xp THEN
+        SELECT state
+        INTO verter_state
+        FROM verter
+        WHERE check_id = NEW.check_id AND state != 'Start'
+        ORDER BY time DESC
+        LIMIT 1;
+
+        IF verter_state = 'Success' OR verter_state IS NULL THEN
+            SELECT state
+            INTO p2p_state
+            FROM p2p
+            WHERE check_id = NEW.check_id AND state != 'Start'
+            ORDER BY time DESC
+            LIMIT 1;
+
+            IF p2p_state = 'Success' THEN
+                RETURN NEW;
+            END IF;
+        END IF;
+    END IF;
+    RETURN NULL;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trg_update_xp
+BEFORE INSERT ON xp
+FOR EACH ROW EXECUTE FUNCTION check_xp_update();
+
+INSERT INTO xp(check_id, xp_amount) VALUES(88, 100); -- will not add
+INSERT INTO xp(check_id, xp_amount) VALUES(1, 10000); -- will not add
+INSERT INTO xp(check_id, xp_amount) VALUES(1, 782); -- will add
+INSERT INTO xp(check_id, xp_amount) VALUES(4, 782); -- will not add
+INSERT INTO xp(check_id, xp_amount) VALUES(2, 500); -- will add
